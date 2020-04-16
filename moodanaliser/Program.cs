3d9a9db2eed54575f34296f26b2210aea9159a6b
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-04-16 20:02:19 +0530

    Added:-welcome to mood analiser

---
+++ moodanaliser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace moodanaliser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
